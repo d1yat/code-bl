23fc935137bb6f518f0494af16f5124317188a44
CODE.ID <CODE.ID@DESKTOP-SDMV22J>
2019-04-05 10:23:17 +0700

    Add .htaccess & base_url() configuration for development / production

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+
+//Development
+$config['base_url'] = 'http://devcode.brotherlabs.id/';
+
+//Production
+//$config['base_url'] = 'http://code.brotherlabs.id/';
 
 /*
 |--------------------------------------------------------------------------
