14e1cd73d4c596439a87a19319c73f973833d022
CODE.ID <CODE.ID@DESKTOP-SDMV22J>
2019-10-10 10:09:33 +0700

    Add Development and Production config

--- application/config/config-production.php
+++ application/config/config-production.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://development.brotherlabs.id/';
+$config['base_url'] = 'http://code.brotherlabs.id/';
 
 /*
 |--------------------------------------------------------------------------
